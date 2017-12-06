d10449acebcecd1cfec536d1ef44ed13b9b332ea
Eugene Zaikonnikov <eugene@funcall.org>
2017-12-06 13:16:52 +0100

    exports

--- package.lisp
+++ package.lisp
@@ -7,5 +7,6 @@
   (:nicknames #:ntp)
   (:export #:synchronize #:get-adjusted-universal-time #:local-stratum #:small-time #:big-time
 	   #:adjusted-big-time #:ntp #:fraction-to-internal #:internal-to-fraction #:to-fraction #:from-fraction
-	   #:+millis+ #:+micros+ #:+nanos+ #:ntp-address #:sub-internal))
+	   #:+millis+ #:+micros+ #:+nanos+ #:ntp-address #:sub-internal #:usec-to-fraction #:fraction-to-usec
+	   #:seconds-to-fraction #:fraction-to-seconds))
 
